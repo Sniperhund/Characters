398b96cf7987130e4de690d3a02e652d2ddb5da5
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-08-26 10:55:42 +0200

    chore: Update grid layout and add max-width to character section

--- assets/ts/util/types.ts
+++ assets/ts/util/types.ts
@@ -1,6 +1,6 @@
 export enum Status {
     Alive = "Alive",
-    Dead = "Dead",
+    Dead = "Deceased",
 }
 
 /* There's only two genders */
@@ -15,20 +15,23 @@ export enum ResideStatus {
 }
 
 export type Character = {
+    id: number
     name: string
-    age: number
+    age?: number
     status: Status
     gender: Gender
     releationship: {
         name: string
         relationship: string
-        image: string
+        image?: string
+        link?: number
     }[]
     resides: {
         name: string
         status: ResideStatus
+        image?: string
     }[]
-    height: number
+    height?: number
     short_description: string
     images: string[]
 }
